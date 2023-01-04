3c593102640f6784a03ee4574703f15f14bffa48
mawad <1190759@student.birzeit.edu>
2023-01-04 14:22:31 +0200

    mawada

---
+++ app/src/main/java/edu/mawada1190759/secondin/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.mawada1190759.secondin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
