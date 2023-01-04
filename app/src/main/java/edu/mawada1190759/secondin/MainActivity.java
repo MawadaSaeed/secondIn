72de6ab79f24794b2e035dc920212e5cd5638cd1
mawad <1190759@student.birzeit.edu>
2023-01-04 15:08:00 +0200

    mawada

--- app/src/main/java/edu/mawada1190759/secondin/MainActivity.java
+++ app/src/main/java/edu/mawada1190759/secondin/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText txtName;
@@ -21,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferences.Editor editor;
     private Books book ;
     private static final String DATA = "DATA";
+    ArrayList <Books> books;
+    int i= 0;
+
 
 
     @Override
@@ -47,15 +52,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void btnAddOnClick(View view) {
 
-        book = new Books(txtName.getText().toString(),
-              txtAuth.getText().toString(), txtPage.getText().toString());
+        books.add(i, new Books(txtName.getText().toString(),
+                txtAuth.getText().toString(), txtPage.getText().toString()));
+        i++;
 
     }
 
     public void btnSaveOnClick(View view) {
 
 
-        String booksString = book.toString();
+        String booksString = books.toString();
 
         editor.putString(DATA, booksString);
         editor.commit();
